8927748802876dc6eed7f16da26b6066e8d707bd
youssefbayome <yousef.bayome258@gmail.com>
2022-06-15 01:40:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]); // where to store the returned data
+  const [error, setError] = useState(null);
+  // where to store the coming errors
+  useEffect(() => {
+    function fetchData() {
+      // the function to fetch data from the api
+      axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => setData(res.data))
+        .catch(error => setError(error))
+      console.log(error)
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <div className='App'>
+      {data.map((item) => (
+        <div className='user-card' key={item.id}>
+          <h1>{item.name}<span>'{item.username}'</span></h1>
+          <p><label>email: </label>{item.email}</p>
+          <p><label>adress: </label>{item.address.street},{item.address.city}</p>
+
+        </div>
+      ))}
     </div>
   );
-}
 
+}
 export default App;
